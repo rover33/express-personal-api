4dd8b263a207931fcf0fec78fd1ac08de3e17638
rover33 <remy.j.pearlstone@gmail.com>
2018-01-03 16:40:35 -0700

    herrro

--- server.js
+++ server.js
@@ -35,13 +35,15 @@ app.get('/', function homepage(req, res) {
  * JSON API Endpoints
  */
 
+
+
 app.get('/api', function api_index(req, res) {
   // TODO: Document all your api endpoints below
   res.json({
-    woops_i_has_forgot_to_document_all_my_endpoints: true, // CHANGE ME ;)
-    message: "Welcome to my personal api! Here's what you need to know!",
-    documentation_url: "https://github.com/example-username/express_self_api/README.md", // CHANGE ME
-    base_url: "http://YOUR-APP-NAME.herokuapp.com", // CHANGE ME
+    woops_i_has_forgot_to_document_all_my_endpoints: false,
+    message: "Welcome to my remy's awesome api",
+    documentation_url: "https://github.com/rover33/express-personal-api/blob/master/README.md",
+    base_url: "https://tranquil-brushlands-55213.herokuapp.com/",
     endpoints: [
       {method: "GET", path: "/api", description: "Describes all available endpoints"},
       {method: "GET", path: "/api/profile", description: "Data about me"}, // CHANGE ME
